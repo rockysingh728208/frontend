a63f04419ca38cf1703dbd5ac043a1d38984c398
vidya sagar <sagarkashyap9155@gmail.com>
2025-08-03 22:10:24 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,8 +17,8 @@ const Login = () => {
 
     try {
       const url = state === 'Sign Up'
-        ? 'https://backendvidya.onrender.com/api/user/register'
-        : 'https://backendvidya.onrender.com/api/user/login';
+        ? 'https://backend-sagar.onrender.com/api/user/register'
+        : 'https://backend-sagar.onrender.com/api/user/login';
 
       const { data } = await axios.post(url, { name, email, password });
 
